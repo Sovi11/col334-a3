39be699737f488ba1a259d397fbefa1aaaa624f0
granadierfc <125267796+granadierfc@users.noreply.github.com>
2023-11-01 17:20:06 +0530

    Update AIMD-2.py

--- AIMD-2.py
+++ AIMD-2.py
@@ -6,6 +6,12 @@ import random
 import matplotlib.pyplot as plt
 random.seed(int(time.time()))
 
+
+def dbg1(x):
+    print(f"LINE NUMBER = {x}")
+def dbg(x):
+    print(f"{x}")
+
 def get_offset(input_string):
     L = input_string.split("\n", 3)
     z = L[0]
@@ -21,72 +27,48 @@ def parse_input(input_string):
         x = x + L[i]
     return x
 
-
-
+srvr_ip = '127.0.0.1'
 # srvr_ip = "10.17.7.134"
-srvr_ip = "127.0.0.1"
-srvr_port = 9801
+srvr_port = 9802
 sa = (srvr_ip, srvr_port)
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def send_size():
-    return "SendSize\n\n"
+def ask_size():
+    payload_size = "SendSize\nReset\n\n".encode("utf-8")
+    udp.sendto(payload_size, sa)
+    udp.settimeout(0.1)
+    try:
+        message1, _ = udp.recvfrom(1024)
+        print(f"HERER")
+    except:
+        print(f"JK")
+        return ask_size()
+    return int((str(message1)[7:][:-5]))
+
 
 def request_data(offset, number_of_bytes):
     message = f"Offset: {offset}\nNumBytes: {number_of_bytes}\n\n"
     return message
 
-# RESETING.
-message1 = "Reset\n\n"
-udp.sendto(message1.encode(), sa)
 
-udp.settimeout(0.0016)
-
+udp.settimeout(0.1)
 #****************************************************************************************#
-
-message1 = send_size()
-T1 = time.time()
-for _ in range(10):
-    udp.sendto(message1.encode(), sa)
-while(1):
-    try:
-        data, server = udp.recvfrom(2048)
-        T2 = time.time() - T1
-        break
-    except socket.timeout:
-        print(f"waiting for packet.")
+message1 = ask_size()
 
 time.sleep(0.5)
-
-k = 5
+k = 10
 strt = time.time()
 flag1 = False
-rtt = T2/1.2
-delta= rtt/3
-
-#****************************************************************************************#
-
-# udp.settimeout(rtt/2)
-# while(not(flag1)):
-#     try:
-#         message1 = send_size()
-#         udp.sendto(message1.encode(), sa) 
-#         data, server = udp.recvfrom(2048)
-#         if((data.decode())[0] == 'O'):
-#             flag1 = True
-#     except socket.timeout:
-#         print("SEND SIZE REQUEST TIMED OUT !!")
-# print(f'THE SEND SIZE REPLY :-> {data}')
-
-#****************************************************************************************#
+rtt = 0.013 ##0.013 vayu
+rtt = 0.0005 ## self.
+delta= 0.7 * rtt
 
 def parse_size(recvd):
     s = recvd[6:]
     return int(s)
 
-#****************************************************************************************#
 
-Total_size = parse_size(data)
+Total_size = message1
 packet_size = 1448
 last_packet_size = Total_size % packet_size
 total_packets = ((Total_size-1)//packet_size) + 1
@@ -97,26 +79,30 @@ message = [""] * total_packets
 for i in range(total_packets):
     queue[i] = total_packets - (i + 1)
 sz = total_packets
-
 #****************************************************************************************#
 
-udp.settimeout(delta/(2)*k)
-while(sz > k):
+
+while(sz > 1):   
+    print(f"CURRENT BURST SIZE = {k}")
+    k = min(sz - 1, k)
     window = []
+    t1 = time.time()
     for _ in range(k):
         message1 = request_data(queue[-1] * packet_size, packet_size)
         udp.sendto(message1.encode('utf-8'), sa)
         window.append(queue[-1])
         queue.pop()
-    
     sz -= k
     cnt = 0
-    time.sleep(rtt/1.1)
-    for _ in range(k):
+    time.sleep(rtt)
+    udp.settimeout(delta/(3)*k)
+    for _ in range((3 * k)//2):
         if(cnt == k):
             break
         try:
-            data, server = udp.recvfrom(2048)       
+            data, server = udp.recvfrom(2048)
+            if(cnt == 0):
+                t2 = time.time()
             h = get_offset(data.decode('utf-8'))
             if(vis[h//packet_size] == False):
                 cnt += 1
@@ -125,48 +111,22 @@ while(sz > k):
         except socket.timeout:
             cnt = cnt
     print(f"effficiency :-> {cnt / k}")
+    new_rtt = t2 - t1
+    next_k = k
+    if(cnt/k > 0.89):
+        next_k += 1
+    elif(cnt/k >= 0.58):
+        next_k *= 2
+        next_k //= 5
+        next_k = max(3, next_k)
+    else:
+        # time.sleep(rtt)
+        next_k = 2
     for i in range(k):
         if(vis[window[i]] == False):
             queue.append(window[i])
             sz += 1
-
-#*************************************************************#
-
-
-# strt3 = time.time()
-# print("hi")
-# print(f"Time taken 1 =: {strt3- strt}")
-
-while(sz > 1):
-    window = []
-    for _ in range(sz - 1):
-        message1 = request_data(queue[-1] * packet_size, packet_size)
-        udp.sendto(message1.encode('utf-8'), sa)
-        window.append(queue[-1])
-        queue.pop()
-    cnt = 0
-    time.sleep(rtt/1.5)
-    x = sz - 1
-    x *= 2
-    for _ in range(x):
-        if(cnt == sz - 1):
-            break
-        try:
-            data, server = udp.recvfrom(2048)       
-            h = get_offset(data.decode('utf-8'))
-            if(vis[h//packet_size] == False):
-                cnt += 1
-                vis[h//packet_size] = True
-                message[h//packet_size] = parse_input(data.decode('utf-8'))
-        except socket.timeout:
-            cnt = cnt
-    for i in range(sz - 1):
-        if(vis[window[i]] == False):
-            queue.append(window[i])
-    sz = len(queue)
-
-# strt4 = time.time()
-# print(f"Time taken 2 =: {strt4- strt}")
+    k = next_k
 
 cnd = True
 udp.settimeout(rtt)
@@ -213,8 +173,8 @@ while(not(flag1)):
         print("send again 3")
 
 print(data)
-# print(Total_size)
+print(Total_size)
 
-# for i in range(total_packets):
-#     if(vis[i] == False):
-#         print(f"failed at  {i}")
+for i in range(total_packets):
+    if(vis[i] == False):
+        print(f"failed at  {i}")
